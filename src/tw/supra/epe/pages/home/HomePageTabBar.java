db484d07470ab7cbc70e257576c552d1e633cb93
Supra.W <supra.cn@gmail.com>
2015-01-05 22:23:14 +0800

    首页的tab风格

---
+++ src/tw/supra/epe/pages/home/HomePageTabBar.java
@@ -0,0 +1,226 @@
+/*
+ * Copyright (C) 2011 The Android Open Source Project
+ * Copyright (C) 2011 Jake Wharton
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package tw.supra.epe.pages.home;
+
+import static android.view.ViewGroup.LayoutParams.MATCH_PARENT;
+import tw.supra.epe.R;
+import android.content.Context;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v4.view.ViewPager.OnPageChangeListener;
+import android.util.AttributeSet;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.viewpagerindicator.IconPagerAdapter;
+import com.viewpagerindicator.PageIndicator;
+
+/**
+ * This widget implements the dynamic action bar tab behavior that can change
+ * across different configurations or circumstances.
+ */
+public class HomePageTabBar extends LinearLayout implements PageIndicator {
+	/** Title text used when no title is provided by the adapter. */
+	private static final CharSequence EMPTY_TITLE = "";
+
+	/**
+	 * Interface for a callback when the selected tab has been reselected.
+	 */
+	public interface OnTabReselectedListener {
+		/**
+		 * Callback when the selected tab has been reselected.
+		 *
+		 * @param position
+		 *            Position of the current center item.
+		 */
+		void onTabReselected(int position);
+	}
+
+	private Runnable mTabSelector;
+
+	private final OnClickListener mTabClickListener = new OnClickListener() {
+		public void onClick(View view) {
+			final int oldSelected = mViewPager.getCurrentItem();
+			final int newSelected = (Integer) view.getTag();
+			mViewPager.setCurrentItem(newSelected);
+			if (oldSelected == newSelected && mTabReselectedListener != null) {
+				mTabReselectedListener.onTabReselected(newSelected);
+			}
+		}
+	};
+
+	private ViewPager mViewPager;
+	private ViewPager.OnPageChangeListener mListener;
+
+	private int mMaxTabWidth;
+	private int mSelectedTabIndex;
+
+	private OnTabReselectedListener mTabReselectedListener;
+
+	public HomePageTabBar(Context context) {
+		this(context, null);
+	}
+
+	public HomePageTabBar(Context context, AttributeSet attrs) {
+		super(context, attrs);
+//		super(context, R.attr.vpiTabPageIndicatorStyle);
+	}
+	
+	public HomePageTabBar(Context context, AttributeSet attrs, int defStyleAttr) {
+		super(context, attrs, defStyleAttr);
+		// TODO Auto-generated constructor stub
+	}
+
+	public void setOnTabReselectedListener(OnTabReselectedListener listener) {
+		mTabReselectedListener = listener;
+	}
+
+	@Override
+	public void onAttachedToWindow() {
+		super.onAttachedToWindow();
+		if (mTabSelector != null) {
+			// Re-post the selector we saved
+			post(mTabSelector);
+		}
+	}
+
+	@Override
+	public void onDetachedFromWindow() {
+		super.onDetachedFromWindow();
+		if (mTabSelector != null) {
+			removeCallbacks(mTabSelector);
+		}
+	}
+
+	private void addTab(int index, CharSequence text, int iconResId) {
+//		final TextView tabView = new TextView(getContext());
+		final TextView tabView = (TextView)View.inflate(getContext(), R.layout.home_tab_bar_item, null);
+		tabView.setTag(index);
+//		tabView.setFocusable(true); 
+		tabView.setOnClickListener(mTabClickListener);
+		tabView.setText(text);
+//		tabView.setGravity(Gravity.CENTER);
+//		tabView.setTextColor(getResources().getColorStateList(R.color.home_page_tab_bar_text_color));
+//		tabView.setBackgroundDrawable(getResources().getDrawable(R.color.home_page_tab_bar_bg_color));
+		
+		int bgRes;
+		if(index == 0){
+			bgRes = R.drawable.home_tab_start_bg;
+		}else if (index == (mViewPager.getAdapter().getCount()-1)) {
+			bgRes = R.drawable.home_tab_end_bg;
+		}else {
+			bgRes = R.drawable.home_tab_bg;
+		}
+		tabView.setBackgroundResource(bgRes);
+
+		addView(tabView, new LinearLayout.LayoutParams(0, MATCH_PARENT, 1));
+	}
+
+	@Override
+	public void onPageScrollStateChanged(int arg0) {
+		if (mListener != null) {
+			mListener.onPageScrollStateChanged(arg0);
+		}
+	}
+
+	@Override
+	public void onPageScrolled(int arg0, float arg1, int arg2) {
+		if (mListener != null) {
+			mListener.onPageScrolled(arg0, arg1, arg2);
+		}
+	}
+
+	@Override
+	public void onPageSelected(int arg0) {
+		setCurrentItem(arg0);
+		if (mListener != null) {
+			mListener.onPageSelected(arg0);
+		}
+	}
+
+	@Override
+	public void setViewPager(ViewPager view) {
+		if (mViewPager == view) {
+			return;
+		}
+		if (mViewPager != null) {
+			mViewPager.setOnPageChangeListener(null);
+		}
+		final PagerAdapter adapter = view.getAdapter();
+		if (adapter == null) {
+			throw new IllegalStateException("ViewPager does not have adapter instance.");
+		}
+		mViewPager = view;
+		view.setOnPageChangeListener(this);
+		notifyDataSetChanged();
+	}
+
+	public void notifyDataSetChanged() {
+		removeAllViews();
+		PagerAdapter adapter = mViewPager.getAdapter();
+		IconPagerAdapter iconAdapter = null;
+		if (adapter instanceof IconPagerAdapter) {
+			iconAdapter = (IconPagerAdapter) adapter;
+		}
+		final int count = adapter.getCount();
+		for (int i = 0; i < count; i++) {
+			CharSequence title = adapter.getPageTitle(i);
+			if (title == null) {
+				title = EMPTY_TITLE;
+			}
+			int iconResId = 0;
+			if (iconAdapter != null) {
+				iconResId = iconAdapter.getIconResId(i);
+			}
+			addTab(i, title, iconResId);
+		}
+		if (mSelectedTabIndex > count) {
+			mSelectedTabIndex = count - 1;
+		}
+		setCurrentItem(mSelectedTabIndex);
+		requestLayout();
+	}
+
+	@Override
+	public void setViewPager(ViewPager view, int initialPosition) {
+		setViewPager(view);
+		setCurrentItem(initialPosition);
+	}
+
+	@Override
+	public void setCurrentItem(int item) {
+		if (mViewPager == null) {
+			throw new IllegalStateException("ViewPager has not been bound.");
+		}
+		mSelectedTabIndex = item;
+		mViewPager.setCurrentItem(item);
+
+		final int tabCount = getChildCount();
+		for (int i = 0; i < tabCount; i++) {
+			final View child = getChildAt(i);
+			final boolean isSelected = (i == item);
+			child.setSelected(isSelected);
+		}
+	}
+
+	@Override
+	public void setOnPageChangeListener(OnPageChangeListener listener) {
+		mListener = listener;
+	}
+}
