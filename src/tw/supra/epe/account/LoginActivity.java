f580cf699be6157a80a40a607a671668b5467632
Supra.W <supra.cn@gmail.com>
2015-02-16 19:36:48 +0800

    log

--- src/tw/supra/epe/account/LoginActivity.java
+++ src/tw/supra/epe/account/LoginActivity.java
@@ -1,4 +1,3 @@
-
 package tw.supra.epe.account;
 
 import tw.supra.epe.R;
@@ -8,64 +7,67 @@ import android.os.Bundle;
 import android.support.v13.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
 
+import com.viewpagerindicator.PageIndicator;
+
 public class LoginActivity extends BaseActivity {
-    private LoginFrag mLoginFrag;
-    private RegFrag mRegFrag;
+	private LoginFrag mLoginFrag;
+	private RegFrag mRegFrag;
 
-    private FragmentStatePagerAdapter mAdapter;
+	private FragmentStatePagerAdapter mAdapter;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-        ViewPager pager = (ViewPager) findViewById(R.id.view_pager);
-        pager.setAdapter(getAdapter());
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_login);
+		ViewPager pager = (ViewPager) findViewById(R.id.view_pager);
+		pager.setAdapter(getAdapter());
+		((PageIndicator) findViewById(R.id.indicator)).setViewPager(pager);
+	}
 
-    private FragmentStatePagerAdapter getAdapter() {
-        if (null == mAdapter) {
-            mAdapter = new FragmentStatePagerAdapter(getFragmentManager()) {
+	private FragmentStatePagerAdapter getAdapter() {
+		if (null == mAdapter) {
+			mAdapter = new FragmentStatePagerAdapter(getFragmentManager()) {
 
-                @Override
-                public BaseFrag getItem(int pos) {
-                    switch (pos) {
-                        case 0:
-                            return getLoginFrag();
-                        case 1:
-                            return getRegFrag();
-                        default:
-                            throw new IllegalStateException(
-                                    "Illegal page postion check out getCount()");
-                    }
-                }
+				@Override
+				public BaseFrag getItem(int pos) {
+					switch (pos) {
+					case 0:
+						return getLoginFrag();
+					case 1:
+						return getRegFrag();
+					default:
+						throw new IllegalStateException(
+								"Illegal page postion check out getCount()");
+					}
+				}
 
-                @Override
-                public int getCount() {
-                    return 2;
-                }
-                
-                @Override
-                public CharSequence getPageTitle(int position) {
-                    return getItem(position).getTitle();
-                }
-            };
+				@Override
+				public int getCount() {
+					return 2;
+				}
 
-        }
-        return mAdapter;
-    }
+				@Override
+				public CharSequence getPageTitle(int position) {
+					return getItem(position).getTitle();
+				}
+			};
 
-    private LoginFrag getLoginFrag() {
-        if (null == mLoginFrag) {
-            mLoginFrag = new LoginFrag();
-        }
-        return mLoginFrag;
-    }
+		}
+		return mAdapter;
+	}
 
-    private RegFrag getRegFrag() {
-        if (null == mRegFrag) {
-            mRegFrag = new RegFrag();
-        }
-        return mRegFrag;
-    }
+	private LoginFrag getLoginFrag() {
+		if (null == mLoginFrag) {
+			mLoginFrag = new LoginFrag();
+		}
+		return mLoginFrag;
+	}
+
+	private RegFrag getRegFrag() {
+		if (null == mRegFrag) {
+			mRegFrag = new RegFrag();
+		}
+		return mRegFrag;
+	}
 
 }
